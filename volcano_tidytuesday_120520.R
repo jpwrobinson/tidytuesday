0045dd3081844939c8128e786b069aa665cfacbe
James Robinson <jpwrobinson@gmail.com>
2020-05-11 19:05:00 +0100

    volcano data

---
+++ volcano_tidytuesday_120520.R
@@ -0,0 +1,10 @@
+setwd('tidytuesday')
+library(tidyverse)
+
+volcano <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-12/volcano.csv')
+eruptions <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-12/eruptions.csv')
+events <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-12/events.csv')
+
+head(volcano) %>% data.frame()
+head(eruptions) %>% data.frame()
+head(events) %>% data.frame()
