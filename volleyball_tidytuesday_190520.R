574241cc3eda101e939ca8e72695120db967a4d0
James Robinson <jpwrobinson@gmail.com>
2020-05-19 17:53:54 +0100

    done

--- volleyball_tidytuesday_190520.R
+++ volleyball_tidytuesday_190520.R
@@ -1,10 +1,11 @@
 # Get the Data
 setwd('tidytuesday')
-vb <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-19/vb_matches.csv', guess_max = 76000)
-
+# vb <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-19/vb_matches.csv', guess_max = 76000)
+library(ggtext)
+library(countrycode)
 library(tidyverse)
 library(cowplot)
-library(ggimage)
+library(ggflags)
 
 theme_set(theme_classic())
 head(vb)
@@ -33,31 +34,43 @@ top<-ggplot(vb_avg) +
   geom_line(aes(year, age_win), col='#1b9e77') +
   geom_point(aes(year, age_lose), col='#d95f02') +
   geom_line(aes(year, age_lose), col='#d95f02') +
-  labs(x = '', y = 'yrs', subtitle='Winners are older (and getting younger)') +
+  annotate('text', 2004, 29.4, label='Winning team', col='#1b9e77') +
+  annotate('text', 2002, 27.2, label='Losing team', col='#d95f02') +
+  labs(x = '', y = 'age, yrs', subtitle='Winners are older (but getting younger)') +
   scale_x_continuous(breaks=seq(2000, 2020,3)) +
   theme( 
-        plot.margin=margin(0.1, 0.1, -1, 0.1, 'cm'))
+        plot.margin=margin(0.1, 0.1, -1, 0.1, 'cm'),
+        text = element_text(family = "Andale Mono"))
 
 mid<-ggplot(vb_avg) + 
   geom_point(aes(year, hgt_win),col='#1b9e77') +
   geom_line(aes(year, hgt_win),col='#1b9e77') +
   geom_point(aes(year, hgt_lose), col='#d95f02') +
   geom_line(aes(year, hgt_lose), col='#d95f02') +
-  labs(x = '', y = 'cm', subtitle='Winners are taller (and getting taller)') +
+  labs(x = '', y = 'height, cm', subtitle='Winners are taller (and growing)',
+    caption = '@jamespwr\n#tidytuesday: Beach volleyball') +
   scale_x_continuous(breaks=seq(2000, 2020,3)) +
   theme(plot.margin=margin(-1, 0.1, 0, 0.1, 'cm'))
 
 
-bot<-ggplot(vb_nation %>% filter(n > 1000), aes(country_win, n)) + 
-  # geom_bar(stat='identity') +
-  geom_flag(size = 10) +
-  labs(x = '', y = '',subtitle='Winning nations') +
+vb_nation$code<-tolower(countrycode(vb_nation$country_win, origin = 'country.name', destination = 'iso2c'))
+
+bot<-ggplot(vb_nation %>% filter(n > 1000), 
+        aes(country_win, n)) + 
+  geom_bar(stat='identity', fill='#7fcdbb') +
+  geom_flag(aes(country=code), size = 10) +
+  labs(x = '', y = 'Games won',
+    title='The FIVB Beach Volleyball World Tour',
+    subtitle='Brazil are the champs (since 2000)') +
   scale_x_discrete(labels=NULL, expand=c(0,0)) +
   scale_y_continuous(labels=scales::comma, expand=c(0,0)) +
-  theme(axis.ticks = element_blank())
+  theme(axis.ticks = element_blank(),
+    plot.margin=margin(0.5, 0.1, 0, 0.1, 'cm'),
+    plot.subtitle=element_text(hjust=0.5),
+    plot.title=element_text(hjust=0.5, face='bold'))
 
-pdf(file='volleyball.pdf', height=5, width=7)
-l<-plot_grid(top, mid, nrow=2, align='hv')
-plot_grid(l, bot, ncol=2)
+pdf(file='volleyball.pdf', height=6, width=9)
+l<-plot_grid(top, mid, ncol=2, align='hv')
+plot_grid(bot, l, nrow=2)
 dev.off()
 
