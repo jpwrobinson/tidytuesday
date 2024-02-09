becc0be4ef120e641835e778b582385abedc20c7
James Robinson <jpwrobinson@gmail.com>
2024-02-09 10:05:22 +0000

    groundhog script

---
+++ groundhogs.R
@@ -0,0 +1,11 @@
+library(tidyverse)
+## tidytuesday for use-R group. 14th Feb 2024
+## James Robinson
+
+
+tuesdata <- tidytuesdayR::tt_load(2024, week = 5)
+
+
+groundhogs <- tuesdata$groundhogs
+predictions <- tuesdata$predictions
+
